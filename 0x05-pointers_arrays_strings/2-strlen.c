98b574edbe7b90dea756c355222f9e62e7dd22c6
Ulrich <ulrichlissa@gmail.com>
2022-06-13 07:37:04 -0700

    My project 0x05 commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *  * _strlen - returns length of string
+ *   * @s: string
+ *    * Return: length of string
+ *     */
+
+int _strlen(char
+		{
+			int len = 0
+
+				while
+					{
+							len++;
+								}
+
+									return
+									}
